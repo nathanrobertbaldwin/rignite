be843b78a9ccebdc029994482e2e58f081eb6643
nathanrobertbaldwin <nathanrobertbaldwin@gmail.com>
2023-07-28 12:15:04 -0500

    Initial commit with starter.

--- backend/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-class Configuration(object):
-    FLASK_ENV = os.environ.get("FLASK_ENV")
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
--- backend/app/models.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-
-class Product(db.Model):
-    __tablename__ = "products"
-    id = db.Column(db.Integer, primary_key=True)
