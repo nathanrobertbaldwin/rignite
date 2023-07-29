94e6a56d3123dd3bc9a62049fa415d8dd5893eea
nathanrobertbaldwin <nathanrobertbaldwin@gmail.com>
2023-07-28 22:12:58 -0500

    Fixing bugs with deploy. ENUM field fixed.

--- migrations/versions/71717af28423_.py
+++ migrations/versions/71717af28423_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 2acbaac0e883
+Revision ID: 71717af28423
 Revises: 
-Create Date: 2023-07-28 22:02:09.419251
+Create Date: 2023-07-28 22:12:29.688224
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '2acbaac0e883'
+revision = '71717af28423'
 down_revision = None
 branch_labels = None
 depends_on = None
