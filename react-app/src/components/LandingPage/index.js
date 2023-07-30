94227ab245b0689f119c1b1231167e7008b51b17
nathanrobertbaldwin <nathanrobertbaldwin@gmail.com>
2023-07-30 10:33:28 -0500

    Pushing changes to routes and home styling.

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -15,32 +15,54 @@ export default function Landing() {
 
   if (!isLoaded) return <></>;
 
+  // categories = {loop through products and grab unique categories};
+
   return (
-    <>{JSON.stringify(products)}</>
-    // <>
-    //     <h1>Shop by Category</h1>
-    //     <div id='catagories'>
-    //         <div id='KB'>
-    //             <img className='cateImg' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Golde33443.jpg/640px-Golde33443.jpg' alt='' />
-    //             <p>KeyBoards   →</p>
-    //         </div>
-    //         <div id='Sp'>
-    //             <img className='cateImg' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Golde33443.jpg/640px-Golde33443.jpg' alt='' />
-    //             <p>Speakers   →</p>
-    //         </div>
-    //         <div id='HP'>
-    //             <img className='cateImg' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Golde33443.jpg/640px-Golde33443.jpg' alt='' />
-    //             <p>Headphones   →</p>
-    //         </div>
-    //         <div id='MP'>
-    //             <img className='cateImg' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Golde33443.jpg/640px-Golde33443.jpg' alt='' />
-    //             <p>Mousepads   →</p>
-    //         </div>
-    //         <div id='Mi'>
-    //             <img className='cateImg' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Golde33443.jpg/640px-Golde33443.jpg' alt='' />
-    //             <p>Mice   →</p>
-    //         </div>
-    //     </div>
-    // </>
+    // <>{JSON.stringify(products)}</>
+    <>
+      <h1>Shop by Category</h1>
+      <div id="catagories">
+        <div id="KB">
+          <img
+            className="cateImg"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Golde33443.jpg/640px-Golde33443.jpg"
+            alt=""
+          />
+          <p>KeyBoards →</p>
+        </div>
+        <div id="Sp">
+          <img
+            className="cateImg"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Golde33443.jpg/640px-Golde33443.jpg"
+            alt=""
+          />
+          <p>Speakers →</p>
+        </div>
+        <div id="HP">
+          <img
+            className="cateImg"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Golde33443.jpg/640px-Golde33443.jpg"
+            alt=""
+          />
+          <p>Headphones →</p>
+        </div>
+        <div id="MP">
+          <img
+            className="cateImg"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Golde33443.jpg/640px-Golde33443.jpg"
+            alt=""
+          />
+          <p>Mousepads →</p>
+        </div>
+        <div id="Mi">
+          <img
+            className="cateImg"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Golde33443.jpg/640px-Golde33443.jpg"
+            alt=""
+          />
+          <p>Mice →</p>
+        </div>
+      </div>
+    </>
   );
 }
