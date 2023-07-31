c9864b9f2da41d8a8fb218119be8131ab937a7dc
nathanrobertbaldwin <nathanrobertbaldwin@gmail.com>
2023-07-30 19:28:38 -0500

    Finished with categories products.

--- react-app/src/components/CategoryProducts/ProductCard/ProductCard.js
+++ react-app/src/components/CategoryProducts/ProductCard/ProductCard.js
@@ -1,19 +1,27 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./ProductCard.css";
 
 export default function ProductCard({ product }) {
+  const photosData = useSelector((store) => store.photos);
+  const photos = Object.values(photosData);
+  const productPhotos = photos.filter((photo) => {
+    return photo.product_id == product.id;
+  });
+  console.log(product);
+
   return (
     <Link id="product_card_container" to={`/product/${product.id}`}>
       <div id="product_card_image_container">
         <img
           alt="product"
           id="product_image"
-          src={product.photo_url}
-          title={product.name}
+          src={productPhotos[0].url}
+          title={product.product_name}
         />
       </div>
       <div id="product_name">
-        <h3>{product.name}</h3>
+        <h3>{product.product_name}</h3>
       </div>
     </Link>
   );
