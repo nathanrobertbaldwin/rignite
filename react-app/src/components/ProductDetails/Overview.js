18b6a56f90a9f215b1d999450b48730db9a38031
JamesAskelson <jamesaskelson630@gmail.com>
2023-08-01 15:57:53 -0500

    finished overview and specs details

--- react-app/src/components/ProductDetails/Overview.js
+++ react-app/src/components/ProductDetails/Overview.js
@@ -1,6 +1,48 @@
+import './Overview.css'
+
 function Overview({product}) {
+    const product_photos = product.product_photos
+    const primaryPhoto = product_photos[0]
+    const secondaryPhoto = product_photos[1]
+    const otherPhotos = product_photos.slice(2);
+    console.log('primary', primaryPhoto)
+    console.log('secondary', secondaryPhoto)
+    console.log('other', otherPhotos)
+
     return (
-        <h2>put overview details here</h2>
+        <div>
+            <div id='overview-description-container'>
+                <h2 className='overview-product-name'>{product.product_name}</h2>
+                <p className='overview-description'>{product.description}</p>
+            </div>
+            <div>
+                <img
+                alt="product"
+                id="overview-primary-image"
+                src={primaryPhoto.url}
+                />
+            </div>
+            <div id='overview-description-container'>
+                <h2 className='overview-product-name'>{product.product_name}</h2>
+                <p className='overview-description'>{product.description}</p>
+            </div>
+            <div>
+            <img
+                alt="product"
+                id="overview-secondary-image"
+                src={secondaryPhoto.url}
+                />
+            </div>
+            <div id='other-photos-container'>
+            {otherPhotos?.map((photo) => (
+                <img
+                alt="product"
+                id="overview-other-images"
+                src={photo.url}
+                />
+            ))}
+            </div>
+        </div>
     )
 }
 
