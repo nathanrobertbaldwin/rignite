ea57ccff37071ed08c223e88a122fd12103ccc56
JamesAskelson <jamesaskelson630@gmail.com>
2023-08-04 14:51:41 -0500

    finished login and signup css plus functionality for going between the two

--- react-app/src/components/SignupFormModal/index.js
+++ react-app/src/components/SignupFormModal/index.js
@@ -2,7 +2,10 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { signUp } from "../../store/session";
+import OpenModalButton from "../OpenModalButton";
+import LoginFormModal from "../LoginFormModal";
 import "./SignupForm.css";
+
 
 function SignupFormModal() {
 	const dispatch = useDispatch();
@@ -44,124 +47,189 @@ function SignupFormModal() {
 	};
 
 	return (
-		<>
-			<h1>Sign Up</h1>
+		<div id='signup-container'>
+			<h1 id='signup-title'>Sign Up</h1>
+			<hr id='signup-hr'/>
 			<form onSubmit={handleSubmit}>
 				<ul>
 					{errors.map((error, idx) => (
 						<li key={idx}>{error}</li>
 					))}
 				</ul>
-				<label>
-					Email
-					<input
-						type="text"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-						required
+				<div id='signup-main-container'>
+					<div id='user-info-container'>
+						<h2 id='signup-subtitle'>
+							User Info
+						</h2>
+						<div id='signup-email-container'>
+							<label>
+								Email
+							</label>
+								<input
+									type="text"
+									value={email}
+									placeholder="Email"
+									onChange={(e) => setEmail(e.target.value)}
+									required
+								/>
+						</div>
+						<div id='signup-username-container'>
+							<label>
+								Username
+							</label>
+								<input
+									type="text"
+									value={username}
+									placeholder="Username"
+									onChange={(e) => setUsername(e.target.value)}
+									required
+								/>
+						</div>
+						<div id='signup-firstname-container'>
+							<label>
+								Firstname
+							</label>
+								<input
+									type="text"
+									value={firstname}
+									placeholder="Firstname"
+									onChange={(e) => setFirstname(e.target.value)}
+									required
+								/>
+						</div>
+						<div id='signup-lastname-container'>
+							<label>
+								Lastname
+							</label>
+								<input
+									type="text"
+									value={lastname}
+									placeholder="Lastname"
+									onChange={(e) => setLastname(e.target.value)}
+									required
+								/>
+						</div>
+					</div>
+					<div id='middle-border'>
+					</div>
+					<div id='shipping-info-container'>
+						<h2 id='signup-subtitle'>
+							Shipping Info
+						</h2>
+						<div id='signup-address-container'>
+							<label>
+								Address
+							</label>
+								<input
+									type="text"
+									value={address}
+									placeholder="Address"
+									onChange={(e) => setAddress(e.target.value)}
+									required
+								/>
+						</div>
+						<div id='signup-city-container'>
+							<label>
+								City
+							</label>
+								<input
+									type="text"
+									value={city}
+									placeholder="City"
+									onChange={(e) => setCity(e.target.value)}
+									required
+								/>
+						</div>
+						<div id='signup-state-container'>
+							<label>
+								State
+							</label>
+								<input
+									type="text"
+									value={state}
+									placeholder="State"
+									onChange={(e) => setState(e.target.value)}
+									required
+								/>
+						</div>
+						<div id='signup-zipcode-container'>
+							<label>
+								Zipcode
+							</label>
+								<input
+									type="number"
+									value={zipcode}
+									placeholder="Zipcode"
+									onChange={(e) => setZipcode(e.target.value)}
+									required
+								/>
+						</div>
+						<div id='signup-admin-container'>
+							<label>
+								Are you an admin?
+							</label>
+								<input
+									type="radio"
+									value={true}
+									name="yes-no"
+									onChange={(e) => setAdmin(e.target.value)}
+									required
+								/> Yes
+								<input
+									type="radio"
+									value={false}
+									name="yes-no"
+									onChange={(e) => setAdmin(e.target.value)}
+									required
+								/> No
+						</div>
+					</div>
+					<div id='middle-border'>
+
+					</div>
+					<div id='email-password-container'>
+						<h2 id='signup-subtitle'>
+							Password
+						</h2>
+						<div id='signup-password-container'>
+							<label>
+								Password
+							</label>
+								<input
+									type="password"
+									value={password}
+									placeholder="Password"
+									onChange={(e) => setPassword(e.target.value)}
+									required
+								/>
+						</div>
+						<div id='signup-confirm-password-container'>
+							<label>
+								Confirm Password
+							</label>
+								<input
+									type="password"
+									value={confirmPassword}
+									placeholder="Confirm Password"
+									onChange={(e) => setConfirmPassword(e.target.value)}
+									required
+								/>
+						</div>
+					</div>
+				</div>
+				<div id='signup-button'>
+					<button type="submit">Sign Up</button>
+				</div>
+				<hr id='signup-hr2'/>
+				<div id='login-signup-container'>
+					<p>Already a member?</p>
+					<OpenModalButton
+					buttonText="Login"
+					modalComponent={<LoginFormModal />}
 					/>
-				</label>
-				<label>
-					Username
-					<input
-						type="text"
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-						required
-					/>
-				</label>
-				<label>
-					Firstname
-					<input
-						type="text"
-						value={firstname}
-						onChange={(e) => setFirstname(e.target.value)}
-						required
-					/>
-				</label>
-				<label>
-					Lastname
-					<input
-						type="text"
-						value={lastname}
-						onChange={(e) => setLastname(e.target.value)}
-						required
-					/>
-				</label>
-				<label>
-					Address
-					<input
-						type="text"
-						value={address}
-						onChange={(e) => setAddress(e.target.value)}
-						required
-					/>
-				</label>
-				<label>
-					City
-					<input
-						type="text"
-						value={city}
-						onChange={(e) => setCity(e.target.value)}
-						required
-					/>
-				</label>
-				<label>
-					State
-					<input
-						type="text"
-						value={state}
-						onChange={(e) => setState(e.target.value)}
-						required
-					/>
-				</label>
-				<label>
-					Zipcode
-					<input
-						type="number"
-						value={zipcode}
-						onChange={(e) => setZipcode(e.target.value)}
-						required
-					/>
-				</label>
-				<label>
-					Are you an admin?
-					<input
-						type="radio"
-						value={true}
-						name="yes-no"
-						onChange={(e) => setAdmin(e.target.value)}
-						required
-					/> Yes
-					<input
-						type="radio"
-						value={false}
-						name="yes-no"
-						onChange={(e) => setAdmin(e.target.value)}
-						required
-					/> No
-				</label>
-				<label>
-					Password
-					<input
-						type="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						required
-					/>
-				</label>
-				<label>
-					Confirm Password
-					<input
-						type="password"
-						value={confirmPassword}
-						onChange={(e) => setConfirmPassword(e.target.value)}
-						required
-					/>
-				</label>
-				<button type="submit">Sign Up</button>
+				</div>
 			</form>
-		</>
+		</div>
 	);
 }
 
