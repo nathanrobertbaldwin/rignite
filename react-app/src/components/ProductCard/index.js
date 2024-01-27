4cd3f1611b073062b6710525bbf84cc834ff6a08
NathanBaldwin <nathanrobertbaldwin@gmail.com>
2024-01-27 14:17:45 -0600

    Fixed manage products button and div-ing

--- react-app/src/components/ProductCard/index.js
+++ react-app/src/components/ProductCard/index.js
@@ -14,32 +14,30 @@ export default function ProductCard({ product }) {
 
   return (
     boolRenderCard && (
-      <Link
-        id="product_card_container"
-        to={`/products/${product.id}`}
-        title={product?.product_name}
-      >
-        <img
-          id="product_card_image"
-          alt="product"
-          src={product?.product_photos[0]?.url}
-          title={product?.product_name}
-        />
-        <div id="product_card_text">
-          <h3 id="product_name_h3">{product?.product_name}</h3>
-          <p>{tagline}</p>
-        </div>
-        {sessionUser?.is_admin && (
-          <div id="manage_product_button_container">
-            <li className="nav_links">
-              <OpenModalButton
-                className="modal_button"
-                buttonText="Manage Product"
-                modalComponent={<EditProductModal product={product} />}
-              />
-            </li>
+      <Link to={`/products/${product.id}`} title={product?.product_name}>
+        <div id="product_card_container">
+          <img
+            id="product_card_image"
+            alt="product"
+            src={product?.product_photos[0]?.url}
+            title={product?.product_name}
+          />
+          <div id="product_text_container">
+            <h3 id="product_name_h3">{product?.product_name}</h3>
+            <div>{tagline}</div>
+            {sessionUser?.is_admin && (
+              <div id="manage_product_button_container">
+                <li className="nav_links">
+                  <OpenModalButton
+                    className="modal_button"
+                    buttonText="Manage Product"
+                    modalComponent={<EditProductModal product={product} />}
+                  />
+                </li>
+              </div>
+            )}
           </div>
-        )}
+        </div>
       </Link>
     )
   );
